08609b703f03d5d5802939b9978c87e3086698ba
Jashu <jb779@njit.edu>
2021-12-02 23:01:33 -0500

    Authenticed task endpoints

--- src/index.js
+++ src/index.js
@@ -13,3 +13,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is running on Port " + port);
 });
+
+const Task = require("./models/task");
+const User = require("./models/user");
+
+const main = async () => {
+  // const task = await Task.findById("61a987a8ed8916e8d623ef48");
+  // await task.populate("owner");
+  // console.log(task.owner);
+
+  const user = await User.findById("61a986cf453eb3ca243ee2c9");
+  await user.populate("tasks");
+  console.log(user.tasks);
+};
+
+main();
